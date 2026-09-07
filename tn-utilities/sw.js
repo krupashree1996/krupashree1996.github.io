a499a37c7620eb882bdcc203177f4bc65c7c8d3d
krupashree1996 <krupashree1996@users.noreply.github.com>
2026-09-07 14:41:21 +0530

    tn-utilities: auto-commit clean bills/receipts, gate buttons on resolved checks

--- tn-utilities/sw.js
+++ tn-utilities/sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'tn-utils-v2';
+var CACHE = 'tn-utils-v3';
 var PRECACHE = ['index.html', 'style.css', 'parser.js', 'calc.js', 'app.js', 'data/bundle.js', 'manifest.json', 'icons/icon-192.png', 'icons/icon-512.png'];
 
 self.addEventListener('install', function (e) {
